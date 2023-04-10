ad4c94097ac95b4243c9af8c81fff264a270c8ec
Isaiah Odhner <isaiahodhner@gmail.com>
2023-04-10 17:29:04 -0400

    Add buttons for Paint tools

--- paint.py
+++ paint.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from enum import Enum
 
 from textual import events
 from textual.app import App, ComposeResult
@@ -6,6 +7,85 @@ from textual.containers import Container
 from textual.css.query import NoMatches
 from textual.reactive import var
 from textual.widgets import Button, Static
+
+class Tool(Enum):
+    """The tools available in the Paint app."""
+    free_form_select = 1
+    select = 2
+    eraser = 3
+    fill = 4
+    pick_color = 5
+    magnifier = 6
+    pencil = 7
+    brush = 8
+    airbrush = 9
+    text = 10
+    line = 11
+    curve = 12
+    rectangle = 13
+    polygon = 14
+    ellipse = 15
+    rounded_rectangle = 16
+
+    def get_icon(self) -> str:
+        """Get the icon for this tool."""
+        # Alternatives considered:
+        # - Free-Form Select:  ✂️📐🆓🕸✨⚝🫥🇫/🇸◌
+        # - Rectangular Select: ⬚▧🔲
+        # - Eraser: 🧼🧽🧹🚫👋🗑️
+        # - Fill Bucket (Flood Fill): 🌊💦💧🌈🎉🎊🪣🫗
+        # - Pick Color: 🎨💉
+        # - Magnifier: 🔍🔎👀🔬🔭🧐🕵️‍♂️🕵️‍♀️
+        # - Pencil: ✏️✍️🖎🖊️🖋️✒️🖆📝🖍️
+        # - Brush: 🖌️🖌👨‍🎨🧑‍🎨💅
+        # - Airbrush: 💨ᖜ╔🧴🥤🫠
+        # - Text: 🆎📝📄📃🔤📜A
+        # - Line: 📏📉📈⟍𝈏⧹
+        # - Curve: ↪️🪝🌙〰️◡◠~∼≈∽∿〜〰﹋﹏≈≋～
+        # - Rectangle: ▭▬▮▯◼️◻️⬜⬛🟧🟩
+        # - Polygon: ▙𝗟𝙇⬣⬟△▲🔺🔻🔳🔲🔷🔶🔴🔵🟠🟡
+        # - Ellipse: ⬭🔴🔵🔶🔷🔸🔹🟠🟡🟢🟣🫧
+        # - Rounded Rectangle: ▢⬜⬛
+        
+        return {
+            Tool.free_form_select: "⚝",
+            Tool.select: "⬚",
+            Tool.eraser: "🧼",
+            Tool.fill: "🫗",
+            Tool.pick_color: "💉",
+            Tool.magnifier: "🔍",
+            Tool.pencil: "✏️",
+            Tool.brush: "🖌️",
+            Tool.airbrush: "💨",
+            Tool.text: "A",
+            Tool.line: "⟍",
+            Tool.curve: "～",
+            Tool.rectangle: "▭",
+            Tool.polygon: "𝙇",
+            Tool.ellipse: "⬭",
+            Tool.rounded_rectangle: "▢",
+        }[self]
+
+    def get_name(self) -> str:
+        """Get the name of this tool."""
+        return {
+            Tool.free_form_select: "Free-Form Select",
+            Tool.select: "Rectangular Select",
+            Tool.eraser: "Eraser",
+            Tool.fill: "Fill Bucket",
+            Tool.pick_color: "Pick Color",
+            Tool.magnifier: "Magnifier",
+            Tool.pencil: "Pencil",
+            Tool.brush: "Brush",
+            Tool.airbrush: "Airbrush",
+            Tool.text: "Text",
+            Tool.line: "Line",
+            Tool.curve: "Curve",
+            Tool.rectangle: "Rectangle",
+            Tool.polygon: "Polygon",
+            Tool.ellipse: "Ellipse",
+            Tool.rounded_rectangle: "Rounded Rectangle",
+        }[self]
 
 
 class PaintApp(App):
@@ -70,6 +150,10 @@ class PaintApp(App):
             yield Button("0", id="number-0")
             yield Button(".", id="point")
             yield Button("=", id="equals", variant="warning")
+            # tool buttons
+            for tool in Tool:
+                yield Button(tool.get_icon(), id=tool.name, variant="primary")
+            
 
     def on_key(self, event: events.Key) -> None:
         """Called when the user presses a key."""
