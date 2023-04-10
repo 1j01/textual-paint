99fcda2c53b79a85436504c5ffae524ec39ce578
Isaiah Odhner <isaiahodhner@gmail.com>
2023-04-10 17:40:59 -0400

    Remove calculator logic/UI

--- paint.py
+++ paint.py
@@ -1,4 +1,3 @@
-from decimal import Decimal
 from enum import Enum
 
 from textual import events
@@ -93,67 +92,22 @@ class PaintApp(App):
 
     CSS_PATH = "paint.css"
 
-    numbers = var("0")
-    show_ac = var(True)
-    left = var(Decimal("0"))
-    right = var(Decimal("0"))
-    value = var("")
-    operator = var("plus")
+    # show_tools_box = var(True)
 
     NAME_MAP = {
-        "asterisk": "multiply",
-        "slash": "divide",
-        "underscore": "plus-minus",
-        "full_stop": "point",
-        "plus_minus_sign": "plus-minus",
-        "percent_sign": "percent",
-        "equals_sign": "equals",
-        "minus": "minus",
-        "plus": "plus",
+        # key to button id
     }
 
-    def watch_numbers(self, value: str) -> None:
-        """Called when numbers is updated."""
-        # Update the Numbers widget
-        self.query_one("#numbers", Static).update(value)
-
-    def compute_show_ac(self) -> bool:
-        """Compute switch to show AC or C button"""
-        return self.value in ("", "0") and self.numbers == "0"
-
-    def watch_show_ac(self, show_ac: bool) -> None:
-        """Called when show_ac changes."""
-        self.query_one("#c").display = not show_ac
-        self.query_one("#ac").display = show_ac
+    # def watch_show_tools_box(self, show_tools_box: bool) -> None:
+    #     """Called when show_tools_box changes."""
+    #     self.query_one("#tools_box").display = not show_tools_box
 
     def compose(self) -> ComposeResult:
         """Add our buttons."""
         with Container(id="paint"):
-            yield Static(id="numbers")
-            yield Button("AC", id="ac", variant="primary")
-            yield Button("C", id="c", variant="primary")
-            yield Button("+/-", id="plus-minus", variant="primary")
-            yield Button("%", id="percent", variant="primary")
-            yield Button("÷", id="divide", variant="warning")
-            yield Button("7", id="number-7")
-            yield Button("8", id="number-8")
-            yield Button("9", id="number-9")
-            yield Button("×", id="multiply", variant="warning")
-            yield Button("4", id="number-4")
-            yield Button("5", id="number-5")
-            yield Button("6", id="number-6")
-            yield Button("-", id="minus", variant="warning")
-            yield Button("1", id="number-1")
-            yield Button("2", id="number-2")
-            yield Button("3", id="number-3")
-            yield Button("+", id="plus", variant="warning")
-            yield Button("0", id="number-0")
-            yield Button(".", id="point")
-            yield Button("=", id="equals", variant="warning")
             # tool buttons
             for tool in Tool:
-                yield Button(tool.get_icon(), id=tool.name, variant="primary")
-            
+                yield Button(tool.get_icon(), id="tool_button_" + tool.name, variant="primary")
 
     def on_key(self, event: events.Key) -> None:
         """Called when the user presses a key."""
@@ -165,15 +119,10 @@ class PaintApp(App):
                 pass
 
         key = event.key
-        if key.isdecimal():
-            press(f"number-{key}")
-        elif key == "c":
-            press("c")
-            press("ac")
-        else:
-            button_id = self.NAME_MAP.get(key)
-            if button_id is not None:
-                press(self.NAME_MAP.get(key, key))
+        
+        button_id = self.NAME_MAP.get(key)
+        if button_id is not None:
+            press(self.NAME_MAP.get(key, key))
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         """Called when a button is pressed."""
@@ -181,48 +130,8 @@ class PaintApp(App):
         button_id = event.button.id
         assert button_id is not None
 
-        def do_math() -> None:
-            """Does the math: LEFT OPERATOR RIGHT"""
-            try:
-                if self.operator == "plus":
-                    self.left += self.right
-                elif self.operator == "minus":
-                    self.left -= self.right
-                elif self.operator == "divide":
-                    self.left /= self.right
-                elif self.operator == "multiply":
-                    self.left *= self.right
-                self.numbers = str(self.left)
-                self.value = ""
-            except Exception:
-                self.numbers = "Error"
-
-        if button_id.startswith("number-"):
-            number = button_id.partition("-")[-1]
-            self.numbers = self.value = self.value.lstrip("0") + number
-        elif button_id == "plus-minus":
-            self.numbers = self.value = str(Decimal(self.value or "0") * -1)
-        elif button_id == "percent":
-            self.numbers = self.value = str(Decimal(self.value or "0") / Decimal(100))
-        elif button_id == "point":
-            if "." not in self.value:
-                self.numbers = self.value = (self.value or "0") + "."
-        elif button_id == "ac":
-            self.value = ""
-            self.left = self.right = Decimal(0)
-            self.operator = "plus"
-            self.numbers = "0"
-        elif button_id == "c":
-            self.value = ""
-            self.numbers = "0"
-        elif button_id in ("plus", "minus", "divide", "multiply"):
-            self.right = Decimal(self.value or "0")
-            do_math()
-            self.operator = button_id
-        elif button_id == "equals":
-            if self.value:
-                self.right = Decimal(self.value)
-            do_math()
+        # if button_id.startswith("tool_button_"):
+        #     self.selected_tool = Tool[button_id[5:]]
 
 
 if __name__ == "__main__":
