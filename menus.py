044b404a0e486e037d0fa8f893e526afe21725e6
Isaiah Odhner <isaiahodhner@gmail.com>
2023-04-12 22:07:38 -0400

    Start making menus work

--- menus.py
+++ menus.py
@@ -27,17 +27,17 @@ class Menu(Container):
         super().__init__(**kwargs)
         self.add_class("menu")
         self.items = items
-        for item in items:
-            if not hasattr(item, "id"):
-                item.id = "menu_item_" + to_snake_case(item.name)
+        self.parent_menu = None
 
-    def compose(self) -> ComposeResult:
-        """Define widget structure."""
-        for item in self.items:
-            if item.type == "separator":
-                yield Static("─" * 20, classes="menu_separator")
-            else:
-                yield Button(item.name, id=item.id, classes="menu_item")
+    def watch_items(self, old_items, new_items: List[str]) -> None:
+        """Update the menu items."""
+        for item in old_items:
+            self.unmount(item)
+        for item in new_items:
+            self.mount(item)
+            if item.submenu:
+                self.app.mount(item.submenu)
+                item.submenu.close()
 
     def on_key(self, event: events.Key) -> None:
         """Called when the user presses a key."""
@@ -50,11 +50,43 @@ class Menu(Container):
             self.focus_index += 1
             if self.focus_index >= len(self.items):
                 self.focus_index = 0
-        elif event.key == "enter":
-            if self.items[self.focus_index].action:
-                self.items[self.focus_index].action()
-            # elif self.items[self.focus_index].submenu:
-            #     self.items[self.focus_index].submenu.open()
+        elif event.key == "escape":
+            self.close()
+            self.parent_menu.focus()
+    
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        """Called when a button is clicked or activated with the keyboard."""
+
+        button_id = event.button.id
+        assert button_id is not None
+
+        if button_id.startswith("menu_item_"):
+            if event.button.action:
+                event.button.action()
+                root_menu = self
+                while root_menu.parent_menu:
+                    root_menu = root_menu.parent_menu
+                root_menu.close()
+            elif event.button.submenu:
+                was_open = event.button.submenu.display
+                for item in self.items:
+                    if item.submenu:
+                        item.submenu.close()
+                if not was_open:
+                    event.button.submenu.open(self)
+
+    def open(self, parent_menu):
+        self.display = True
+        if len(self.items) > 0:
+            self.items[0].focus()
+        self.parent_menu = parent_menu
+    
+    def close(self):
+        for item in self.items:
+            if item.submenu:
+                item.submenu.close()
+        if not isinstance(self, MenuBar):
+            self.display = False
 
 class MenuBar(Menu):
     """A menu bar widget."""
@@ -65,19 +97,24 @@ class MenuBar(Menu):
         self.add_class("menu_bar")
 
 
-class MenuItem:
-    """A menu item."""
+class MenuItem(Button):
+    """A menu item widget."""
 
-    def __init__(self, name: str, action = None, type: str = "item", submenu = None) -> None:
+    def __init__(self, name: str, action = None, id: str = None, submenu = None, **kwargs) -> None:
         """Initialize a menu item."""
-        self.name = name
+        super().__init__(name, **kwargs)
+        self.add_class("menu_item")
         self.action = action
-        self.type = type
         self.submenu = submenu
+        if id:
+            self.id = id
+        else:
+            self.id = "menu_item_" + to_snake_case(name)
 
-class Separator(MenuItem):
-    """A menu separator."""
+class Separator(Static):
+    """A menu separator widget."""
 
-    def __init__(self) -> None:
-        """Initialize a menu separator."""
-        super().__init__("", None, "separator")
+    def __init__(self, **kwargs) -> None:
+        """Initialize a separator."""
+        super().__init__("", **kwargs)
+        self.add_class("separator")
