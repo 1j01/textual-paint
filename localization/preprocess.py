160beeb6ce85c6d3408a106ffc11e50a07e3033d
Isaiah Odhner <isaiahodhner@gmail.com>
2023-04-18 02:23:03 -0400

    Disable code for updating index.html

--- localization/preprocess.py
+++ localization/preprocess.py
@@ -92,11 +92,11 @@ loaded_localizations("{target_lang}", {localizations_json});
     with open(f"{os.path.dirname(__file__)}/{target_lang}/localizations.js", "w", encoding="utf8") as f:
         f.write(js)
 
-file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "index.html"))
-with open(file_path, "r") as f:
-    code = f.read()
-available_langs_json = json.dumps(available_langs).replace('","', '", "')
-code = re.sub(r"(available_languages\s*=\s*)\[[^\]]*\]", f"$1{available_langs_json}]", code)
-with open(file_path, "w") as f:
-    f.write(code)
-print(f'Updated available_languages list in "{file_path}"')
+# file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "index.html"))
+# with open(file_path, "r") as f:
+#     code = f.read()
+# available_langs_json = json.dumps(available_langs).replace('","', '", "')
+# code = re.sub(r"(available_languages\s*=\s*)\[[^\]]*\]", f"$1{available_langs_json}]", code)
+# with open(file_path, "w") as f:
+#     f.write(code)
+# print(f'Updated available_languages list in "{file_path}"')
