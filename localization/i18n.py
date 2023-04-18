40d866280185d6e108192648715e9a460c0444f8
Isaiah Odhner <isaiahodhner@gmail.com>
2023-04-18 15:03:12 -0400

    Track untranslated strings

--- localization/i18n.py
+++ localization/i18n.py
@@ -21,9 +21,21 @@ def load_language(language_code: str):
 	except Exception as e:
 		print(f"Could not load language '{language_code}': {e}")
 
+untranslated = set()
+try:
+	with open("localization/untranslated.txt", "r") as f:
+		untranslated = set(f.read().splitlines())
+except FileNotFoundError:
+	pass
+
 def get(base_language_str: str) -> str:
 	"""Get a localized string."""
 	if base_language_str in translations:
 		return translations[base_language_str]
 	else:
+		if base_language_str not in untranslated:
+			untranslated.add(base_language_str)
+			# append to untranslated strings file
+			with open("localization/untranslated.txt", "a") as f:
+				f.write(base_language_str + "\n")
 		return base_language_str
