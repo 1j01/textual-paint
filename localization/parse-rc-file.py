6a943389bed96ef166c3f10ad31a7032146fff18
Isaiah Odhner <isaiahodhner@gmail.com>
2023-04-18 00:37:48 -0400

    Convert localization JavaScript code to Python using ChatGPT

---
+++ localization/parse-rc-file.py
@@ -0,0 +1,72 @@
+def parse_rc_file(rc_file_text, callback=None, lang=None):
+    strings = []
+    menu = dialog = stringtable = False
+    block_level = 0
+    id_str = dialog_id = hint = orig_str = None
+
+    for line in rc_file_text.splitlines():
+        norm_line = line.strip().replace(r'[\t ]+', ' ').replace(r'\/\/.*$', '')
+
+        if norm_line.endswith(' MENU'):
+            menu = True
+
+        dialog_match = re.match(r'^(\w+) (DIALOG|DIALOGEX) ', norm_line)
+        if dialog_match:
+            dialog_id = dialog_match.group(1)
+            dialog = True
+
+        if norm_line == 'STRINGTABLE':
+            stringtable = True
+
+        if norm_line == 'BEGIN':
+            block_level += 1
+
+        if norm_line == 'END':
+            block_level -= 1
+            if block_level == 0:
+                menu = dialog = dialog_id = stringtable = None
+
+        if dialog and not block_level:
+            match = re.match(r'^[\t ]*(CAPTION)[\t ]+(L?"(.*?("")*)*?")', line)
+            if match:
+                id_str = f"{dialog_id}:{match.group(1)}"
+                hint = f"{dialog_id} {match.group(1)}"
+                orig_str = match.group(2)
+
+        elif (menu or dialog) and block_level:
+            match = re.match(r'^[\t ]*(\w+)[\t ]+(L?"([^"]*?("")*)*?")(,[\t ]*(\w+)){0,1}', line)
+            if match:
+                id_str = match.group(6)
+                hint = f"{match.group(1)} {match.group(6)}" if match.group(6) else match.group(1)
+                orig_str = match.group(2)
+
+        else:
+            match = re.search(r'(L?"(.*)")', line)
+            if match:
+                orig_str = match.group(0)
+            else:
+                match = re.match(r'^[\t ]*(\w+)[\t ]*(\/\/.*)*$', line)
+                if match:
+                    id_str = match.group(1)
+                else:
+                    match = re.match(r'^[\t ]*(L?"(.*)")', line)
+                    if id_str and match:
+                        hint = id_str
+                        orig_str = match.group(1)
+                    else:
+                        id_str = None
+
+        if orig_str:
+            str = orig_str
+
+            wide = str.startswith('L')
+            str = re.sub(r'^L?"(.*)"$', r'\1', str)
+            str = str.replace(r'\r', '\r').replace(r'\n', '\n').replace(r'\t', '\t').replace(r'\\"', '"')
+            if wide:
+                str = re.sub(r'\\x([0-9a-fA-F]{4})', lambda match: chr(int(match.group(1), 16)), str)
+
+            strings.append(str)
+
+            id_str = hint = orig_str = None
+
+    return strings
---
+++ localization/preprocess.py
@@ -0,0 +1,90 @@
+import os
+import re
+import glob
+import json
+
+from parse_rc_file import parse_rc_file
+
+
+base_lang = "en"
+available_langs = [dir for dir in os.listdir(os.path.dirname(__file__)) if re.match(r"^\w+(-\w+)?$", dir)]
+target_langs = [lang for lang in available_langs if lang != base_lang]
+
+print("Target languages:", target_langs)
+
+
+def index_of_hotkey(text):
+    # Returns the index of the ampersand that defines a hotkey, or -1 if not present.
+    return f" {text}".find(re.compile(r"[^&]&[^&\s]"))
+
+
+def has_hotkey(text):
+    return index_of_hotkey(text) != -1
+
+
+def remove_hotkey(text):
+    return re.sub(r"\s?\(&.\)", "", text).replace(re.compile(r"([^&]|^)&([^&\s])"), r"\1\2")
+
+
+def remove_ellipsis(string):
+    return string.replace("...", "")
+
+
+def only_unique(value, index, self):
+    return self.index(value) == index
+
+
+def get_strings(lang):
+    return [parse_rc_file(open(rc_file, "r", encoding="utf16").read().replace("\ufeff", "")) for rc_file in glob.glob(f"{os.path.dirname(__file__)}/{lang}/**/*.rc")]
+
+
+base_strings = get_strings(base_lang)
+for target_lang in target_langs:
+    target_strings = get_strings(target_lang)
+    localizations = {}
+
+    def add_localization(base_string, target_string, fudgedness):
+        localizations[base_string] = localizations.get(base_string, [])
+        localizations[base_string].append({"target_string": target_string, "fudgedness": fudgedness})
+
+    def add_localizations(base_strings, target_strings):
+        for i, target_string in enumerate(target_strings):
+            base_string = base_strings[i]
+            if base_string != target_string and base_string and target_string:
+                splitter = re.compile(r"\t|\r?\n")
+                if splitter.search(base_string):
+                    add_localizations(base_string.split(splitter), target_string.split(splitter))
+                else:
+                    add_localization(remove_ellipsis(base_string), remove_ellipsis(target_string), 1)
+                    if has_hotkey(base_string):
+                        add_localization(remove_ellipsis(remove_hotkey(base_string)), remove_ellipsis(remove_hotkey(target_string)), 3)
+
+    add_localizations(base_strings, target_strings)
+
+    for base_string, options in localizations.items():
+        options.sort(key=lambda x: x["fudgedness"])
+        unique_strings = list(set(option["target_string"] for option in options))
+        if len(unique_strings) > 1:
+            print(f'Collision for "{base_string}": {json.dumps(unique_strings, indent="\t")}')
+        localizations[base_string] = unique_strings[0]
+
+    js = f"""
+//
+// NOTE: This is a generated file! Don't edit it directly.
+// Eventually community translation will be set up on some translation platform.
+// 
+// Generated with: npm run update-localization
+//
+loaded_localizations("{target_lang}", {json.dumps(localizations, indent="\t")});
+"""
+
+    with open(f"{os.path.dirname(__file__)}/{target_lang}/localizations.js", "w", encoding="utf8") as f:
+        f.write(js)
+
+file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "index.html"))
+with open(file_path, "r") as f:
+    code = f.read()
+code = re.sub(r"(available_languages\s*=\s*)\[[^\]]*\]", f"$1{json.dumps(available_langs).replace('","', '", "')}]", code)
+with open(file_path, "w") as f:
+    f.write(code)
+print(f'Updated available_languages list in "{file_path}"')
