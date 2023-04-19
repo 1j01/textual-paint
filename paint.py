0026c06268451e79bd9eb0540991d4c2773decbb
Isaiah Odhner <isaiahodhner@gmail.com>
2023-04-19 19:58:07 -0400

    Fix inconsistent indentation

--- paint.py
+++ paint.py
@@ -263,35 +263,35 @@ class Tool(Enum):
 
 
 palette = [
-	"rgb(0,0,0)", # Black
-	"rgb(128,128,128)", # Dark Gray
-	"rgb(128,0,0)", # Dark Red
-	"rgb(128,128,0)", # Pea Green
-	"rgb(0,128,0)", # Dark Green
-	"rgb(0,128,128)", # Slate
-	"rgb(0,0,128)", # Dark Blue
-	"rgb(128,0,128)", # Lavender
-	"rgb(128,128,64)",
-	"rgb(0,64,64)",
-	"rgb(0,128,255)",
-	"rgb(0,64,128)",
-	"rgb(64,0,255)",
-	"rgb(128,64,0)",
+    "rgb(0,0,0)", # Black
+    "rgb(128,128,128)", # Dark Gray
+    "rgb(128,0,0)", # Dark Red
+    "rgb(128,128,0)", # Pea Green
+    "rgb(0,128,0)", # Dark Green
+    "rgb(0,128,128)", # Slate
+    "rgb(0,0,128)", # Dark Blue
+    "rgb(128,0,128)", # Lavender
+    "rgb(128,128,64)",
+    "rgb(0,64,64)",
+    "rgb(0,128,255)",
+    "rgb(0,64,128)",
+    "rgb(64,0,255)",
+    "rgb(128,64,0)",
 
-	"rgb(255,255,255)", # White
-	"rgb(192,192,192)", # Light Gray
-	"rgb(255,0,0)", # Bright Red
-	"rgb(255,255,0)", # Yellow
-	"rgb(0,255,0)", # Bright Green
-	"rgb(0,255,255)", # Cyan
-	"rgb(0,0,255)", # Bright Blue
-	"rgb(255,0,255)", # Magenta
-	"rgb(255,255,128)",
-	"rgb(0,255,128)",
-	"rgb(128,255,255)",
-	"rgb(128,128,255)",
-	"rgb(255,0,128)",
-	"rgb(255,128,64)",
+    "rgb(255,255,255)", # White
+    "rgb(192,192,192)", # Light Gray
+    "rgb(255,0,0)", # Bright Red
+    "rgb(255,255,0)", # Yellow
+    "rgb(0,255,0)", # Bright Green
+    "rgb(0,255,255)", # Cyan
+    "rgb(0,0,255)", # Bright Blue
+    "rgb(255,0,255)", # Magenta
+    "rgb(255,255,128)",
+    "rgb(0,255,128)",
+    "rgb(128,255,255)",
+    "rgb(128,128,255)",
+    "rgb(255,0,128)",
+    "rgb(255,128,64)",
 ]
 
 class ToolsBox(Container):
