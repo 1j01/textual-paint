c037e1bde4dfc9eb771933b8fc80fadf46cbaa70
Isaiah Odhner <isaiahodhner@gmail.com>
2023-04-30 21:37:21 -0400

    Add metadata

--- src/textual_paint/__init__.py
+++ src/textual_paint/__init__.py
@@ -1,2 +1,12 @@
+"""MS Paint for the terminal, built with Textual."""
+
+__author__ = "Isaiah Odhner"
+__copyright__ = "Copyright Isaiah Odhner"
+__credits__ = ["Isaiah Odhner"]
+__maintainer__ = "Isaiah Odhner"
+__email__ = "isaiahodhner@gmail.com"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
 # For relative imports to work in Python 3.6
 import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
