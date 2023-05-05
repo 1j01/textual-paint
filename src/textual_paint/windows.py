5ba2ad5cd37685e6dbf7fdd095c7feea6c7419d6
Isaiah Odhner <isaiahodhner@gmail.com>
2023-05-05 15:57:54 -0400

    Fix flash of incorrect layout while windows are opening

--- src/textual_paint/windows.py
+++ src/textual_paint/windows.py
@@ -138,12 +138,12 @@ class Window(Container):
         """Called when the widget is mounted."""
         self.mount(self.title_bar)
         self.mount(self.content)
-        # Set focus. (In the future, some windows will not want default focus...)
-        self.focus()
-        # Fix for incorrect layout that would only resolve on mouse over
-        # (I peaked into mouse over handling and it calls update_styles.)
-        # This can still briefly show the incorrect layout, since it relies on a timer.
-        self.set_timer(0.01, lambda: self.app.update_styles(self))
+        with self.app.batch_update():
+            # Set focus. (In the future, some windows will not want default focus...)
+            self.focus()
+            # Fix for incorrect layout that would only resolve on mouse over
+            # (I peaked into mouse over handling and it calls update_styles.)
+            self.app.update_styles(self)
 
     def on_focus(self, event: events.Focus) -> None:
         """Called when the window is focused."""
