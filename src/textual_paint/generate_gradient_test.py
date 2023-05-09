de625b2d9beb76ce291e7fc81ffd221b6db77ed7
Isaiah Odhner <isaiahodhner@gmail.com>
2023-05-09 01:39:53 -0400

    Arrange flowers in a spiral

--- src/textual_paint/generate_gradient_test.py
+++ src/textual_paint/generate_gradient_test.py
@@ -10,38 +10,40 @@ RESET = CSI + '0m'
 # Glyphs to use for the gradient
 GLYPHS = ['🌸', '🌷', '🌹', '🌺', '🌻', '🌼']
 
+def spiral(m):
+    r = math.sqrt(m[0]**2 + m[1]**2) * .05
+    a = math.atan2(m[1], m[0])
+    v = math.sin(100. * (math.sqrt(r) - 0.02 * a))
+    return max(0., min(1., v))
+
 def generate_ansi_art(width, height, file):
     # Calculate the center coordinates of the image
     center_x = width // 2
     center_y = height // 2
 
-    # Calculate the maximum distance from the center to the corner
-    max_distance = math.sqrt(center_x**2 + center_y**2)
-
     for y in range(height):
         for x in range(width):
-            # Calculate the angle from the center to the current position
-            angle = math.atan2(y - center_y, x - center_x)
-            
-            # Calculate the distance from the current position to the center
-            distance = math.sqrt((x - center_x)**2 + (y - center_y)**2)
+            # Calculate the coordinates relative to the center
+            rel_x = x - center_x
+            rel_y = y - center_y
 
-            # Calculate the normalized distance
-            normalized_distance = distance / max_distance
+            # Evaluate the spiral function
+            scale = 0.1
+            v = spiral((rel_x * scale, rel_y * scale))
 
-            # Calculate the color based on normalized distance
-            r = int(normalized_distance * 255)
-            g = int((1 - normalized_distance) * 255)
-            b = int(normalized_distance * 255)
+            # Calculate the color based on the spiral value
+            r = int(v * 255)
+            g = int(((1 - v) * 255)*0.8)
+            b = int(v * 255)
 
             # Generate the truecolor escape code
             # background:
             color = CSI + f'48;2;{r};{g};{b}m'
             # and foreground:
-            color += CSI + f'38;2;{r/10};{g/10};{b/10}m'
+            color += CSI + f'38;2;{r//10};{g//10};{b//10}m'
 
             # Calculate the index of the glyph based on visual weight
-            glyph_index = int((angle / (2 * math.pi)) * (len(GLYPHS) - 1))
+            glyph_index = int(v * (len(GLYPHS) - 1))
 
             # Write the colored glyph to the file
             file.write(color + GLYPHS[glyph_index])
