ec3a930a5e45d231b71871ccda56b1fc77a4e8b0
Isaiah Odhner <isaiahodhner@gmail.com>
2023-05-23 01:41:03 -0400

    Fucksake, I still can't do a transparent background

--- src/textual_paint/edit_colors.py
+++ src/textual_paint/edit_colors.py
@@ -90,6 +90,7 @@ class ColorGrid(Container):
         self.selection_ring.styles.offset = (focused.virtual_region.x - 1, focused.virtual_region.y - 1)
         self.selection_ring.styles.width = focused.region.width + 2
         self.selection_ring.styles.height = focused.region.height + 2
+        self.selection_ring.display = True
 
         self.selected_color = self._color_by_button[focused]
     
@@ -120,6 +121,7 @@ class ColorGrid(Container):
         self.focus_ring.styles.offset = (target_button.virtual_region.x - 1, target_button.virtual_region.y - 1)
         self.focus_ring.styles.width = target_button.region.width + 2
         self.focus_ring.styles.height = target_button.region.height + 2
+        self.focus_ring.display = True
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         """Called when a button is clicked or activated with the keyboard."""
