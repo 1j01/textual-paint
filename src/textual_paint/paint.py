812d628c42d42fbcfaf12c1f4ac86497a3ae7f1b
Isaiah Odhner <isaiahodhner@gmail.com>
2023-06-09 15:11:27 -0400

    Add tooltips to tool buttons

--- src/textual_paint/paint.py
+++ src/textual_paint/paint.py
@@ -419,9 +419,14 @@ class ToolsBox(Container):
         """Add our buttons."""
         self.tool_by_button: dict[Button, Tool] = {}
         for tool in Tool:
-            # TODO: tooltip with tool.get_name()
             button = Button(tool.get_icon(), classes="tool_button")
             button.can_focus = False
+            # TODO: ideally, position tooltip centered under the tool button,
+            # so that it never obscures the tool icon you're hovering over,
+            # and make it appear immediately if a tooltip was already visible
+            # (tooltip should hide and delay should return if moving to a button below,
+            # to allow for easy scanning of the buttons, but not if moving above or to the side)
+            button.tooltip = tool.get_name()
             self.tool_by_button[button] = tool
             yield button
     
