86753aee5efad17af05a6928338f29e82d112b3c
Isaiah Odhner <isaiahodhner@gmail.com>
2023-06-27 18:05:10 -0400

    WIP: recent files (ChatGPT code)
    ChatGPT notes:
    
    > You can use the add_recent_file function to add a new recent file to the list by providing the URI, MIME type, and optional groups. The get_recent_files function returns the list of recent files. Both functions handle the file locking and unlocking using fcntl.lockf to ensure safe concurrent access.
    > 
    > Note: This implementation assumes that the ~/.recently-used file already exists and is a valid XML file following the provided specification. It does not handle cases such as file not found, permission errors, or invalid XML format during reading or writing. You may need to add appropriate error handling based on your specific requirements.

---
+++ src/textual_paint/recent_files.py
@@ -0,0 +1,101 @@
+"""
+Implements the Recent File Storage Specification
+
+https://specifications.freedesktop.org/recent-file-spec/recent-file-spec-0.2.html
+
+TODO: use the Registry on Windows
+"""
+
+import os
+import time
+import fcntl
+import xml.etree.ElementTree as ET
+
+RECENT_FILE_PATH = os.path.expanduser("~/.recently-used")
+MAX_RECENT_FILES = 500
+
+
+def add_recent_file(uri, mime_type, groups=None):
+    # Load recent files
+    recent_files = load_recent_files()
+
+    # Check if the file already exists in the recent files
+    existing_item = find_recent_file_by_uri(recent_files, uri)
+    if existing_item:
+        existing_item.find("Timestamp").text = str(int(time.time()))
+        if groups:
+            groups_elem = existing_item.find("Groups")
+            for group in groups:
+                group_elem = ET.SubElement(groups_elem, "Group")
+                group_elem.text = group
+    else:
+        # Create new recent item
+        recent_item = ET.Element("RecentItem")
+        ET.SubElement(recent_item, "URI").text = uri
+        ET.SubElement(recent_item, "Mime-Type").text = mime_type
+        ET.SubElement(recent_item, "Timestamp").text = str(int(time.time()))
+
+        if groups:
+            groups_elem = ET.SubElement(recent_item, "Groups")
+            for group in groups:
+                group_elem = ET.SubElement(groups_elem, "Group")
+                group_elem.text = group
+
+        # Add the new recent item to the list
+        recent_files.append(recent_item)
+
+    # Truncate the recent files list if necessary
+    if len(recent_files) > MAX_RECENT_FILES:
+        recent_files = recent_files[-MAX_RECENT_FILES:]
+
+    # Save the recent files
+    save_recent_files(recent_files)
+
+
+def get_recent_files():
+    # Load and return the recent files
+    return load_recent_files()
+
+
+def load_recent_files():
+    # Lock the recent file for reading
+    with open(RECENT_FILE_PATH, "r") as file:
+        fcntl.lockf(file, fcntl.LOCK_SH)
+        try:
+            # Parse the XML document
+            root = ET.parse(file).getroot()
+            return root.findall("RecentItem")
+        except ET.ParseError:
+            # Return an empty list if the file is empty or invalid
+            return []
+        finally:
+            # Unlock the recent file after reading
+            fcntl.lockf(file, fcntl.LOCK_UN)
+
+
+def save_recent_files(recent_files):
+    # Lock the recent file for writing
+    with open(RECENT_FILE_PATH, "w") as file:
+        fcntl.lockf(file, fcntl.LOCK_EX)
+        try:
+            # Create the root element
+            root = ET.Element("RecentFiles")
+
+            # Add each recent item to the root element
+            for item in recent_files:
+                root.append(item)
+
+            # Write the XML document to the file
+            file.write("<?xml version=\"1.0\"?>\n")
+            file.write(ET.tostring(root, encoding="utf-8").decode())
+        finally:
+            # Unlock the recent file after writing
+            fcntl.lockf(file, fcntl.LOCK_UN)
+
+
+def find_recent_file_by_uri(recent_files, uri):
+    for item in recent_files:
+        uri_elem = item.find("URI")
+        if uri_elem is not None and uri_elem.text == uri:
+            return item
+    return None
