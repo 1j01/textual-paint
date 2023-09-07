11939b6603568d045e68b597d9a637cc7de78e62
Isaiah Odhner <isaiahodhner@gmail.com>
2023-09-07 17:43:14 -0400

    Resize screenshots for some tests

--- tests/test_snapshots.py
+++ tests/test_snapshots.py
@@ -7,8 +7,11 @@ APPS_DIR = Path("../src/textual_paint")
 PAINT = APPS_DIR / "paint.py"
 GALLERY = APPS_DIR / "gallery.py"
 
+LARGER = (81, 38)
+"""Large enough to show the entire paint app."""
+
 def test_paint_app(snap_compare):
-    assert snap_compare(PAINT)
+    assert snap_compare(PAINT, terminal_size=LARGER)
 
 def test_paint_stretch_skew_dialog(snap_compare):
     assert snap_compare(PAINT, press=["ctrl+w"])
@@ -20,13 +23,13 @@ def test_paint_image_attributes_dialog(snap_compare):
     assert snap_compare(PAINT, press=["ctrl+e"])
 
 def test_paint_open_dialog(snap_compare):
-    assert snap_compare(PAINT, press=["ctrl+o"])
+    assert snap_compare(PAINT, press=["ctrl+o"], terminal_size=LARGER)
 
 def test_paint_save_dialog(snap_compare):
-    assert snap_compare(PAINT, press=["ctrl+s"])
+    assert snap_compare(PAINT, press=["ctrl+s"], terminal_size=LARGER)
 
 def test_paint_help_dialog(snap_compare):
-    assert snap_compare(PAINT, press=["f1"])
+    assert snap_compare(PAINT, press=["f1"], terminal_size=LARGER)
 
 def test_paint_view_bitmap(snap_compare):
     assert snap_compare(PAINT, press=["ctrl+f"])
