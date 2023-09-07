f8f2d5ea0190cb690a7fdb8836a516d480e07443
Isaiah Odhner <isaiahodhner@gmail.com>
2023-09-07 17:21:20 -0400

    Add snapshot tests for more keyboard-accessible dialogs

--- tests/test_snapshots.py
+++ tests/test_snapshots.py
@@ -13,6 +13,27 @@ def test_paint_app(snap_compare):
 def test_paint_stretch_skew(snap_compare):
     assert snap_compare(PAINT, press=["ctrl+w"])
 
+def test_paint_flip_rotate_dialog(snap_compare):
+    assert snap_compare(PAINT, press=["ctrl+r"])
+
+def test_paint_image_attributes_dialog(snap_compare):
+    assert snap_compare(PAINT, press=["ctrl+e"])
+
+def test_paint_open_dialog(snap_compare):
+    assert snap_compare(PAINT, press=["ctrl+o"])
+
+def test_paint_save_dialog(snap_compare):
+    assert snap_compare(PAINT, press=["ctrl+s"])
+
+def test_paint_help_dialog(snap_compare):
+    assert snap_compare(PAINT, press=["f1"])
+
+def test_paint_view_bitmap(snap_compare):
+    assert snap_compare(PAINT, press=["ctrl+f"])
+
+def test_paint_invert_and_exit(snap_compare):
+    assert snap_compare(PAINT, press=["ctrl+i", "ctrl+q"])
+
 def test_gallery_app(snap_compare):
     assert snap_compare(GALLERY)
 
