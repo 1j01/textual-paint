961e9a1560b4ebd7a1913796a7b4c6e3bb2e195a
Isaiah Odhner <isaiahodhner@gmail.com>
2023-09-09 16:26:06 -0400

    WIP trying to get stepwise undo working (and complicating things)

--- tests/pilot_recorder.py
+++ tests/pilot_recorder.py
@@ -1,8 +1,10 @@
 import os
+from typing import Callable
 from textual.css.query import NoMatches, TooManyMatches
 from textual.dom import DOMNode
 from textual.events import Event, Key, MouseDown, MouseMove, MouseUp
 from textual.geometry import Offset
+from textual.pilot import Pilot
 from textual.screen import Screen
 from textual_paint.paint import PaintApp
 
@@ -11,7 +13,6 @@ def unique_file(path: str) -> str:
     counter = 1
 
     while os.path.exists(path):
-        # path = f"{filename} ({counter}){extension}"
         path = f"{filename}_{counter}{extension}"
         counter += 1
 
@@ -38,7 +39,6 @@ def get_selector(target: DOMNode) -> tuple[str, int|None]:
     try:
         query_result = app.query_one(selector)
     except TooManyMatches:
-        # raise Exception(f"Selector {selector!r} matches more than one widget ({app.query(selector).nodes!r})")
         return selector, app.query(selector).nodes.index(target)
         # smarter differentiators would be nice, like tooltip or text content,
         # but at least with indices, you'll know when you changed the tab order
@@ -56,38 +56,57 @@ async def on_event(self: PaintApp, event: Event) -> None:
         widget, _ = self.get_widget_at(*event.screen_offset)
         offset = event.screen_offset - widget.region.offset
         steps.append((event, offset, *get_selector(widget)))
-        # This doesn't hold:
-        # assert event.x == event.screen_x - widget.region.x, f"event.x ({event.x}) should be event.screen_x ({event.screen_x}) - widget ({widget!r}).region.x ({widget.region.x})"
-        # assert event.y == event.screen_y - widget.region.y, f"event.y ({event.y}) should be event.screen_y ({event.screen_y}) - widget ({widget!r}).region.y ({widget.region.y})"
-        # I think the offset == screen_offset once it's bubbled up to the app?
     elif isinstance(event, Key):
         if event.key == "ctrl+z":
             steps.pop()
-            replay()
+            run() # restart the app to replay up to this point
         elif event.key == "ctrl+c":
             save_replay()
 
 app: PaintApp | None = None
+next_after_exit: Callable[[], None] | None = None
 
-def replay() -> None:
+async def async_exec(code: str) -> object:
+    # Make an async function with the code and `exec` it
+    exec(
+        f"async def async_exec_code(): " +
+        "".join(f"\n {line}" for line in code.splitlines()),
+    )
+
+    # Get `async_exec_code` from local variables, call it and return the result
+    return await locals()['async_exec_code']()
+
+async def replay_steps() -> None:
     global app
+    assert app is not None, "app should be set by now"
+    # for event, offset, selector, index in steps:
+    #     ...
+    pilot = Pilot(app)
+    await pilot._wait_for_screen()
+    await async_exec(get_replay_code())
+
+def run() -> None:
+    global app, next_after_exit
+    def startup_and_replay() -> None:
+        global app, next_after_exit
+        app = PaintApp()
+        app.on_event = on_event.__get__(app)
+        
+        app.call_later(replay_steps)
+        app.run() # blocking
+        if next_after_exit:
+            next_after_exit()
     if app is not None:
+        next_after_exit = startup_and_replay
         app.exit()
-    app = PaintApp()
-    app.on_event = on_event.__get__(app)
-    async def replay_steps() -> None:
-        assert app is not None, "app should be set by now"
-        for event, offset, selector, index in steps:
-            await app.on_event(event)
-    app.call_later(replay_steps)
-    app.run() # blocking
+    else:
+        startup_and_replay()
 
 def indent(text: str, spaces: int) -> str:
     return "\n".join(" " * spaces + line for line in text.splitlines())
 
-def save_replay() -> None:
-    assert app is not None, "app should be set by now"
-    helpers_code = ""
+
+def get_replay_code() -> str:
     steps_code = ""
     for event, offset, selector, index in steps:
         if isinstance(event, MouseDown):
@@ -97,7 +116,20 @@ def save_replay() -> None:
                 steps_code += f"widget = pilot.app.query({selector!r})[{index!r}]\n"
                 # can't pass a widget to pilot.click, only a selector, or None
                 steps_code += f"await pilot.click(offset=Offset({offset.x}, {offset.y}) + widget.region.offset)\n"
+        elif isinstance(event, MouseMove):
+            # TODO: generate code for drags (but not other mouse movement)
+            pass
+        elif isinstance(event, MouseUp):
+            pass
+        elif isinstance(event, Key):
+            steps_code += f"await pilot.press({event.key!r})\n"
+        else:
+            raise Exception(f"Unexpected event type {type(event)}")
+    return steps_code
 
+
+def save_replay() -> None:
+    assert app is not None, "app should be set by now"
 
     script = f"""\
 from pathlib import Path, PurePath
@@ -127,8 +159,7 @@ Input.cursor_blink = False  # type: ignore
 
 def test_paint_something(snap_compare: SnapCompareType):
     async def test_paint_something_steps(pilot: Pilot[None]):
-{indent(helpers_code, 8)}
-{indent(steps_code, 8)}
+{indent(get_replay_code(), 8)}
 
     assert snap_compare(PAINT, run_before=test_paint_something_steps, terminal_size=({app.size.width}, {app.size.height}))
 """
@@ -137,4 +168,4 @@ def test_paint_something(snap_compare: SnapCompareType):
     # app.exit(None, Text(f"Saved replay to {OUTPUT_FILE}"))
 
 if __name__ == "__main__":
-    replay() # with no steps, this will just run the app, ready for recording
+    run()
