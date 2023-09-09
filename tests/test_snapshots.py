d2300c96a33a3a344f6274151571cde604486035
Isaiah Odhner <isaiahodhner@gmail.com>
2023-09-08 23:38:37 -0400

    Annotate tests with type hints

--- tests/test_snapshots.py
+++ tests/test_snapshots.py
@@ -1,11 +1,23 @@
-from pathlib import Path
-from typing import TYPE_CHECKING
+from pathlib import Path, PurePath
+from typing import TYPE_CHECKING, Awaitable, Callable, Iterable, Protocol
 
 import pytest
 from textual.pilot import Pilot
 from textual.widgets import Input
 
 from textual_paint import paint
+
+
+class SnapCompareType(Protocol):
+    """Type of the function returned by the snap_compare fixture."""
+    def __call__(
+        self,
+        app_path: str | PurePath,
+        press: Iterable[str] = (),
+        terminal_size: tuple[int, int] = (80, 24),
+        run_before: Callable[[Pilot], Awaitable[None] | None] | None = None,  # type: ignore
+    ) -> bool:
+        ...
 
 # These paths are treated as relative to this file.
 APPS_DIR = Path("../src/textual_paint")
@@ -27,7 +39,7 @@ Input.cursor_blink = False  # type: ignore
     {"theme": "light", "ascii_only": True},
     {"theme": "dark", "ascii_only": True},
 ], ids=lambda param: f"{param['theme']}_{'ascii' if param['ascii_only'] else 'unicode'}")
-def each_theme(request):
+def each_theme(request: pytest.FixtureRequest):
     """Fixture to test each combination of UI styles."""
     theme = request.param.get("theme")
     ascii_only = request.param.get("ascii_only")
@@ -41,40 +53,40 @@ def each_theme(request):
     args.ascii_only = False
 
 
-def test_paint_app(snap_compare, each_theme):
+def test_paint_app(snap_compare: SnapCompareType, each_theme: None):
     assert snap_compare(PAINT, terminal_size=LARGER)
 
-def test_paint_stretch_skew_dialog(snap_compare, each_theme):
+def test_paint_stretch_skew_dialog(snap_compare: SnapCompareType, each_theme: None):
     assert snap_compare(PAINT, press=["ctrl+w"])
 
-def test_paint_flip_rotate_dialog(snap_compare, each_theme):
+def test_paint_flip_rotate_dialog(snap_compare: SnapCompareType, each_theme: None):
     assert snap_compare(PAINT, press=["ctrl+r"])
 
-def test_paint_image_attributes_dialog(snap_compare, each_theme):
+def test_paint_image_attributes_dialog(snap_compare: SnapCompareType, each_theme: None):
     assert snap_compare(PAINT, press=["ctrl+e"])
 
-def test_paint_open_dialog(snap_compare, each_theme):
+def test_paint_open_dialog(snap_compare: SnapCompareType, each_theme: None):
     assert snap_compare(PAINT, press=["ctrl+o"], terminal_size=LARGER)
 
-def test_paint_save_dialog(snap_compare, each_theme):
+def test_paint_save_dialog(snap_compare: SnapCompareType, each_theme: None):
     assert snap_compare(PAINT, press=["ctrl+s"], terminal_size=LARGER)
 
-def test_paint_help_dialog(snap_compare, each_theme):
+def test_paint_help_dialog(snap_compare: SnapCompareType, each_theme: None):
     assert snap_compare(PAINT, press=["f1"], terminal_size=LARGER)
 
-def test_paint_view_bitmap(snap_compare):
+def test_paint_view_bitmap(snap_compare: SnapCompareType):
     assert snap_compare(PAINT, press=["ctrl+f"])
 
-def test_paint_invert_and_exit(snap_compare, each_theme):
+def test_paint_invert_and_exit(snap_compare: SnapCompareType, each_theme: None):
     assert snap_compare(PAINT, press=["ctrl+i", "ctrl+q"])
 
-def test_swap_selected_colors(snap_compare):
-    async def swap_selected_colors(pilot: Pilot):
+def test_swap_selected_colors(snap_compare: SnapCompareType):
+    async def swap_selected_colors(pilot: Pilot[None]):
         await pilot.click("CharInput", control=True)
 
     assert snap_compare(PAINT, run_before=swap_selected_colors)
 
-def test_paint_character_picker_dialog(snap_compare, each_theme):
+def test_paint_character_picker_dialog(snap_compare: SnapCompareType, each_theme: None):
     async def open_character_picker(pilot: Pilot[None]):
         # app.dark = True caused it to fail to open the dialog in the dark theme,
         # due to `self.call_later(self.refresh_css)` in `watch_dark` in `App`
@@ -87,7 +99,7 @@ def test_paint_character_picker_dialog(snap_compare, each_theme):
 
     assert snap_compare(PAINT, run_before=open_character_picker, terminal_size=LARGER)
 
-def test_paint_edit_colors_dialog(snap_compare, each_theme):
+def test_paint_edit_colors_dialog(snap_compare: SnapCompareType, each_theme: None):
     async def open_edit_colors(pilot: Pilot[None]):
         await pilot.pause(1.0) # see comment in test_paint_character_picker_dialog
         pilot.app.query("ColorsBox Button")[0].id = "a_color_button"
@@ -97,7 +109,7 @@ def test_paint_edit_colors_dialog(snap_compare, each_theme):
 
     assert snap_compare(PAINT, run_before=open_edit_colors, terminal_size=LARGEST)
 
-def test_paint_expand_canvas_dialog(snap_compare, each_theme):
+def test_paint_expand_canvas_dialog(snap_compare: SnapCompareType, each_theme: None):
     async def paste_large_content(pilot: Pilot[None]):
         if TYPE_CHECKING:
             # Will be a different class at runtime, per test, due to re-evaluating the module.
@@ -106,7 +118,7 @@ def test_paint_expand_canvas_dialog(snap_compare, each_theme):
 
     assert snap_compare(PAINT, run_before=paste_large_content, terminal_size=LARGER)
 
-def test_paint_error_dialog(snap_compare, each_theme):
+def test_paint_error_dialog(snap_compare: SnapCompareType, each_theme: None):
     async def show_error(pilot: Pilot[None]):
         if TYPE_CHECKING:
             # Will be a different class at runtime, per test, due to re-evaluating the module.
@@ -121,7 +133,7 @@ def test_paint_error_dialog(snap_compare, each_theme):
 
     assert snap_compare(PAINT, run_before=show_error)
 
-def test_paint_custom_zoom_dialog(snap_compare, each_theme):
+def test_paint_custom_zoom_dialog(snap_compare: SnapCompareType, each_theme: None):
     async def show_custom_zoom(pilot: Pilot[None]):
         if TYPE_CHECKING:
             # Will be a different class at runtime, per test, due to re-evaluating the module.
@@ -130,7 +142,7 @@ def test_paint_custom_zoom_dialog(snap_compare, each_theme):
 
     assert snap_compare(PAINT, run_before=show_custom_zoom)
 
-def test_paint_about_paint_dialog(snap_compare, each_theme):
+def test_paint_about_paint_dialog(snap_compare: SnapCompareType, each_theme: None):
     async def show_about_paint(pilot: Pilot[None]):
         if TYPE_CHECKING:
             # Will be a different class at runtime, per test, due to re-evaluating the module.
@@ -139,6 +151,6 @@ def test_paint_about_paint_dialog(snap_compare, each_theme):
 
     assert snap_compare(PAINT, run_before=show_about_paint)
 
-def test_gallery_app(snap_compare):
+def test_gallery_app(snap_compare: SnapCompareType):
     assert snap_compare(GALLERY)
 
