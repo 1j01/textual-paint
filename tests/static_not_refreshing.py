6cc4cb1324eed0b64a26a287b63529f2c6fac991
Isaiah Odhner <isaiahodhner@gmail.com>
2023-09-12 13:40:07 -0400

    WHY????

--- tests/static_not_refreshing.py
+++ tests/static_not_refreshing.py
@@ -3,7 +3,6 @@ from typing import Any, Callable
 
 from textual.app import App, ComposeResult
 from textual.events import Event, Key, MouseMove
-from textual.geometry import Offset
 from textual.widgets import Static
 
 original_on_event = App.on_event  # type: ignore
@@ -19,40 +18,22 @@ class Repro():
         self.app: App[Any] | None = None
         self.next_after_exit: Callable[[], None] | None = None
 
-        self.mouse_position: Offset | None = None
-
-        self.static = Static()
-        self.static.styles.dock = "right"
-        self.static.styles.width = 46
-        self.static.styles.height = "100%"
-
-        repro = self
-        async def on_event(self: App[Any], event: Event) -> None:
-            # Every event seems to be received twice, once with _forwarded set and once without.
-            # I don't claim to understand the forwarding scheme, but ignoring either
-            # the forwarded or the un-forwarded events seems workable.
-            if not event._forwarded:
-                repro.handle_event(event)
-            await original_on_event(self, event)
-        self.app_on_event = on_event
+        # repro = self
+        # async def on_event(self: App[Any], event: Event) -> None:
+        #     # Every event seems to be received twice, once with _forwarded set and once without.
+        #     # I don't claim to understand the forwarding scheme, but ignoring either
+        #     # the forwarded or the un-forwarded events seems workable.
+        #     if not event._forwarded:
+        #         repro.handle_event(event)
+        #     await original_on_event(self, event)
+        # self.app_on_event = on_event
 
     def handle_event(self, event: Event) -> None:
         """Record the mouse position and restart on Ctrl+R."""
         assert self.app is not None, "app should be set when receiving an event from it"
-        if isinstance(event, MouseMove):
-            self.mouse_position = event.screen_offset
-            self.update_static()
-        elif isinstance(event, Key):
+        if isinstance(event, Key):
             if event.key == "ctrl+r":
                 self.run()  # restart the app
-
-    def update_static(self) -> None:
-        assert self.app is not None, "app should be set when updating the Static"
-        if self.static.parent != self.app.screen:
-            self.app.screen.mount(self.static)
-
-        text = f"mouse_position = {self.mouse_position!r}\nrandom_id = {uuid.uuid4().hex!r}"
-        self.static.update(text)
 
     def run(self) -> None:
         """Start or restart the app."""
@@ -60,7 +41,7 @@ class Repro():
             """Start the app, and hook its events."""
             self.next_after_exit = None  # important to allowing you to exit; don't keep launching the app
             self.app = self.app_class()
-            self.app.on_event = self.app_on_event.__get__(self.app)
+            # self.app.on_event = self.app_on_event.__get__(self.app)
             self.app.run()
             # run is blocking, so this will happen after the app exits
             if self.next_after_exit:
@@ -74,13 +55,29 @@ class Repro():
             startup_and_hook()
 
 class ReproApp(App[None]):
-    """App that does nothing, but has instructions and a tooltip."""
+    """App shows the mouse position, has instructions and a tooltip."""
     def compose(self) -> ComposeResult:
         yield Static("Move the mouse to show and update the Static on the right.\n\nThen press Ctrl+R to restart, and note how it no longer updates most of the time.\n\n\n\n")
         tipper = Static("[Hover me for a tooltip.]")
         tipper.tooltip = "A tooltip magically causes a repaint when it appears."
         yield tipper
-
+        self.static = Static("This will show the mouse position...")
+        yield self.static
+        self.static.styles.dock = "right"
+        self.static.styles.width = 46
+        self.static.styles.height = "100%"
+        # self.set_interval(0.1, self.update_static)
+    
+    def on_mouse_move(self, event: MouseMove) -> None:
+        """Show the mouse position in the title."""
+        # self.mouse_position = event.screen_offset
+        self.update_static()
+    
+    def update_static(self) -> None:
+        text = f"mouse_position = {self.mouse_position!r}\nrandom_id = {uuid.uuid4().hex!r}"
+        self.static.update(text)
+    
 
 if __name__ == "__main__":
-    Repro(ReproApp).run()
+    # Repro(ReproApp).run()
+    ReproApp().run()
