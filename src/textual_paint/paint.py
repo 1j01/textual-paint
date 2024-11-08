fcc0329dae5bc8b007ced877a1fff94067a6d13d
Isaiah Odhner <isaiahodhner@gmail.com>
2024-11-08 18:51:55 -0500

    Create cursor at top left of viewport

--- src/textual_paint/paint.py
+++ src/textual_paint/paint.py
@@ -1631,6 +1631,7 @@ Columns: {len(self.palette) // 2}
         def do_the_paste() -> None:
             self.stop_action_in_progress()
             # paste at top left corner of viewport
+            # TODO: adjust formula to match cursor creation
             x: int = max(0, min(self.image.width - 1, int(self.editing_area.scroll_x // self.magnification)))
             y: int = max(0, min(self.image.height - 1, int(self.editing_area.scroll_y // self.magnification)))
             self.image.selection = Selection(Region(x, y, pasted_image.width, pasted_image.height))
@@ -3042,8 +3043,33 @@ Columns: {len(self.palette) // 2}
                 self.selected_tool = Tool.text
                 # Ensure focus is not on the CharInput
                 self.app.set_focus(None)
-                # Create an initial cursor
-                self.image.selection = Selection(Region(0, 0, 1, 1))
+                # Create an initial cursor, at top left corner of viewport
+                # print(self.canvas.offset, self.canvas.scroll_offset, self.canvas.content_offset, self.editing_area.content_offset)
+                # print("self.canvas.styles.margin", self.canvas.styles.margin, "self.canvas.container_viewport", self.canvas.container_viewport)
+                # offset = self.canvas.region.offset - self.editing_area.virtual_region.offset
+                offset = self.canvas.virtual_region.offset
+                # print("offset", offset)
+                # corner_x = int((self.editing_area.scroll_x - self.canvas.styles.margin.left) // self.magnification)
+                # corner_y = int((self.editing_area.scroll_y - self.canvas.styles.margin.top) // self.magnification)
+                # corner_x: int = max(0, min(self.image.width - 1, int(self.editing_area.scroll_x // self.magnification)))
+                # corner_y: int = max(0, min(self.image.height - 1, int(self.editing_area.scroll_y // self.magnification)))
+                # print("self.editing_area.scroll_x / self.magnification", self.editing_area.scroll_x / self.magnification, "self.editing_area.scroll_y / self.magnification", self.editing_area.scroll_y / self.magnification)
+                # print("self.editing_area.scroll_x / self.magnification - offset.x", self.editing_area.scroll_x / self.magnification - offset.x, "self.editing_area.scroll_y / self.magnification - offset.y", self.editing_area.scroll_y / self.magnification - offset.y)
+
+                # corner_x: int = max(0, min(self.image.width - 1, int(self.editing_area.scroll_x / self.magnification) - offset.x))
+                # corner_y: int = max(0, min(self.image.height - 1, int(self.editing_area.scroll_y / self.magnification) - offset.y))
+                # corner_x: int = max(0, min(self.image.width - 1, int(math.ceil(self.editing_area.scroll_x / self.magnification) - offset.x)))
+                # corner_y: int = max(0, min(self.image.height - 1, int(math.ceil(self.editing_area.scroll_y / self.magnification) - offset.y)))
+                corner_x: int = int(self.editing_area.scroll_x - offset.x)
+                corner_y: int = int(self.editing_area.scroll_y - offset.y)
+                # print("corner_x", corner_x, "corner_y", corner_y)
+                corner_x = math.ceil(corner_x / self.magnification)
+                corner_y = math.ceil(corner_y / self.magnification)
+                # print("corner_x", corner_x, "corner_y", corner_y)
+                corner_x = max(0, min(self.image.width - 1, corner_x))
+                corner_y = max(0, min(self.image.height - 1, corner_y))
+                # print("corner_x", corner_x, "corner_y", corner_y)
+                self.image.selection = Selection(Region(corner_x, corner_y, 1, 1))
                 self.image.selection.textbox_mode = True
                 self.image.selection.cursor_mode = True
                 self.image.selection.copy_from_document(self.image)
